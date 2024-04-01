b26e2a83abbc7308e6b3a7c9123935036017172a
pkhurt <peter.hurt@aol.de>
2024-04-01 22:30:21 +0200

    some additions.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
 import tkinter
 
+
 def button_click():
     print("Button Clicked!")
 
+def create_window():
+    root = tkinter.Tk()
+    root.title("Welcome to the Drum Practice Helper")
+    root.geometry('350x200')
+    return root
 
 
 if __name__ == "__main__":
     print("Drum Practice Helper started.")
 
-    # Create a main window
-    root = tkinter.Tk()
-    root.title("Simple GUI")
+    root = create_window()
 
     # Create a button
-    button = tkinter.Button(root, text="Click Me!", command=button_click)
-    button.pack(pady=10, padx=20)  # Add some padding around the button
+    create_random_patter_button = tkinter.Button(root, text="Create Random Drum Letters", command=button_click)
+    # create_random_patter_button.pack(pady=10, padx=20)  
+    create_random_patter_button.grid(column=1, row=3)
 
-    # Run the application
+
+
     root.mainloop()
